f3b5b4fc3c49591aa97eac33dfce2faaefa5e360
maugalcst <maugal.cst@gmail.com>
2026-03-08 08:47:54 -0600

    move MongoDB connection string to secrets manager

--- obj/Debug/net10.0/DependencyInjection.AssemblyInfo.cs
+++ obj/Debug/net10.0/DependencyInjection.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("0156aa56-2693-4d7c-9994-0d7d844afe21")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DependencyInjection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e8b7fc68bb2a460a109948a887215a06c15e5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67f5c7763447bb26b16b3ceceee4cab104c97e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DependencyInjection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DependencyInjection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
